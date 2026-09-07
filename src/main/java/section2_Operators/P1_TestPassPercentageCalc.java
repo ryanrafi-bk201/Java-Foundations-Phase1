2c98b632bf76c465679a4ac77e0c638434c48e69
Mohammad Ryan Ur Rafi <ryan.rafi@therapservices.net>
2026-09-07 10:37:07 +0600

    second problem init

---
+++ src/main/java/section2_Operators/P1_TestPassPercentageCalc.java
@@ -0,0 +1,24 @@
+package section2_Operators;
+
+public class P1_TestPassPercentageCalc {
+    public static void main(String[] args) {
+        int passed = 84;
+        int failed = 12;
+        int skipped = 4;
+        int total = passed + failed + skipped;
+        boolean threshold = false;
+
+        double passedPercentage = (double) passed / total * 100;
+        double failedPercentage = (double) failed / total * 100;
+
+        System.out.println("Total test count: " + total);
+        System.out.println("Passed: " + String.format("%.2f", passedPercentage) + "%");
+        System.out.println("Failed: " + String.format("%.2f", failedPercentage) + "%");
+
+        if (passedPercentage >= 90) {
+            threshold = true;
+        }
+
+        System.out.println("Threshold: " + threshold);
+    }
+}
